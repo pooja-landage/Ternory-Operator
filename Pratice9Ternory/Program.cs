dc4e86582ae1101225138bf1bae0e37b0a07cec3
pooja-landage <poojalandage22@gmail.com>
2021-09-16 08:02:42 +0530

    Add project files.

---
+++ Pratice9Ternory/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Pratice9Ternory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("============Tenory Operator=============");
+            int x = 2, y = 10;
+
+            var result = x > y ? "x is greater than y" : "x is less than y";
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
